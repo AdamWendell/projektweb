edfb83c428cfd1544db34267ee7f3ba36f60d567
Adam Wendell <adam@websmith.se>
2017-03-21 13:52:02 +0100

    Updated server to serve the frontend.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("hej")
+var express = require('express')  
+var path = require('path')
+var compression = require('compression')
+var app = express()
+app.use(compression())
+app.use(express.static(__dirname + '/../frontend/dist'));
+app.get('*', function (req, res) {
+    res.sendFile(path.resolve(__dirname + '/../frontend/dist/index.html'))
+})
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!')
+})
